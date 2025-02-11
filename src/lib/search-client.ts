1e9e02adf8b7f39c3a58a1a9d0f5360514e091b0
Ujjawal-04 <ujjawal0406@gmail.com>
2025-02-11 16:54:47 +0530

    commit

--- src/lib/search-client.ts
+++ src/lib/search-client.ts
@@ -5,7 +5,7 @@ const endpoint = process.env.NEXT_PUBLIC_SEARCH_ENDPOINT || "https://getmeilimei
 
 const apiKey = process.env.NEXT_PUBLIC_SEARCH_API_KEY || "h73yzvic1wjxfv1o0hb6csnt1i3nvksr";
 
-const ms = instantMeiliSearch(endpoint, apiKey)
+const ms = instantMeiliSearch("https://ms-ba238a36478c-18696.fra.meilisearch.io", "5f2852ead3ef2f89ad535218652841a2882dcdd0")
 export const searchClient = (ms.searchClient as unknown) as SearchClient
 
 export const SEARCH_INDEX_NAME =

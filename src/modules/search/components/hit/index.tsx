eadd5ed15e69aa232024e5d150adb0fba76cda26
Vansh Khaneja <vanshkhaneja08@gmail.com>
2025-02-14 22:33:08 +0530

    make search items clickable

--- src/modules/search/components/hit/index.tsx
+++ src/modules/search/components/hit/index.tsx
@@ -25,26 +25,26 @@ const Hit = ({ hit }: HitProps) => {
       href={`/products/${hit.handle}`}
       data-testid="search-result"
     >
-      <div
-        key={hit.id}
-        className="flex sm:flex-col gap-2 w-full p-4 bg-white rounded-lg hover:shadow-elevation-card-hover items-center sm:justify-center"
+<div
+  key={hit.id}
+  className="flex sm:flex-col gap-1 w-full p-2 bg-white rounded-lg items-center sm:justify-center"
+>
+  <Thumbnail 
+    thumbnail={hit.thumbnail}
+    size="square"
+    className="group h-16 w-16 sm:h-[200px] sm:w-[200px] !border-0" // Added !border-0
+  />
+  <div className="flex flex-col justify-between group py-1">
+    <div className="flex flex-col">
+      <Text
+        className="text-ui-fg-subtle"
+        data-testid="search-result-title"
       >
-        <Thumbnail
-          thumbnail={hit.thumbnail}
-          size="square"
-          className="group h-12 w-12 sm:h-full sm:w-full"
-        />
-        <div className="flex flex-col justify-between group">
-          <div className="flex flex-col">
-            <Text
-              className="text-ui-fg-subtle"
-              data-testid="search-result-title"
-            >
-              {hit.title}
-            </Text>
-          </div>
-        </div>
-      </div>
+        {hit.title}
+      </Text>
+    </div>
+  </div>
+</div>
     </LocalizedClientLink>
   )
 }

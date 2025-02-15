975475c2a3d17281bd270b905af482dadf428fa6
Vansh Khaneja <134163979+vansh-khaneja@users.noreply.github.com>
2025-02-15 19:41:37 +0530

    Update index.tsx

--- src/modules/search/components/hit/index.tsx
+++ src/modules/search/components/hit/index.tsx
@@ -1,5 +1,4 @@
-import { Container, Text } from "@medusajs/ui"
-
+import { Text } from "@medusajs/ui"
 import Thumbnail from "@modules/products/components/thumbnail"
 import LocalizedClientLink from "@modules/common/components/localized-client-link"
 import { HttpTypes } from "@medusajs/types"
@@ -25,26 +24,26 @@ const Hit = ({ hit }: HitProps) => {
       href={`/products/${hit.handle}`}
       data-testid="search-result"
     >
-<div
-  key={hit.id}
-  className="flex sm:flex-col gap-1 w-full p-2 bg-white rounded-lg items-center sm:justify-center"
->
-  <Thumbnail 
-    thumbnail={hit.thumbnail}
-    size="square"
-    className="group h-16 w-16 sm:h-[200px] sm:w-[200px] !border-0" // Added !border-0
-  />
-  <div className="flex flex-col justify-between group py-1">
-    <div className="flex flex-col">
-      <Text
-        className="text-ui-fg-subtle"
-        data-testid="search-result-title"
+      <div
+        key={hit.id}
+        className="flex flex-col w-full p-2 bg-white rounded-lg items-center justify-center"
       >
-        {hit.title}
-      </Text>
-    </div>
-  </div>
-</div>
+        <Thumbnail
+          thumbnail={hit.thumbnail}
+          size="square"
+          className="group h-32 w-32 sm:h-[200px] sm:w-[200px] !border-0"
+        />
+        <div className="flex flex-col justify-between group py-1 text-center">
+          <div className="flex flex-col">
+            <Text
+              className="text-ui-fg-subtle"
+              data-testid="search-result-title"
+            >
+              {hit.title}
+            </Text>
+          </div>
+        </div>
+      </div>
     </LocalizedClientLink>
   )
 }

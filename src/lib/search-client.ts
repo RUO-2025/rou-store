d218af36cbc81b3e7b293dd2acae6c9c38ff608d
Shubham Choudhary <130033575+shubhamchoudhary-2003@users.noreply.github.com>
2025-07-14 00:12:11 +0530

    Update search-client.ts

--- src/lib/search-client.ts
+++ src/lib/search-client.ts
@@ -5,7 +5,7 @@ const endpoint = process.env.NEXT_PUBLIC_SEARCH_ENDPOINT || "https://getmeilimei
 
 const apiKey = process.env.NEXT_PUBLIC_SEARCH_API_KEY || "xji5cki34sgwdk2ky3usb0ei4sqhq5w6";
 
-const ms = instantMeiliSearch("https://getmeilimeilisearchv190-production-dc02.up.railway.app", "xji5cki34sgwdk2ky3usb0ei4sqhq5w6")
+const ms = instantMeiliSearch("https://meilisearch-t84ocokkcgskgcc0ogk48w8s.mersate.com", "8VhrnovVY2y59lqO1icIEtw3vUPpDCox")
 export const searchClient = (ms.searchClient as unknown) as SearchClient
 
 export const SEARCH_INDEX_NAME =
